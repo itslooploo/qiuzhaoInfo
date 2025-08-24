c4ec4b9ac00c2c514bd74d3422a7340edb36eef9
loop <loop18638738415@gmail.com>
2025-08-24 22:57:53 +0800

    qiuzhao

--- exam/First/test.java
+++
@@ -1,8 +0,0 @@
-package haut.exam.First;
-
-/**
- * @author loop
- * @version 1.0
- */
-public class test {
-}
--- exam/Test.java
+++
@@ -1,8 +0,0 @@
-package haut.exam;
-
-/**
- * @author loop
- * @version 1.0
- */
-public class Test {
-}
